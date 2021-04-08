c32ad6b0034cc5133bb10fb33703e18e087736ce
tfoerster1211 <thomas.foerster@positiveminds.io>
2021-04-08 16:35:35 +0200

    FAO-15 Changed FAO AWS account ID

---
+++ .circleci/ci.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#########################################
+# Shortcut for ci calls
+#
+# ./ci.sh <integration> lint
+# ./ci.sh <integration> unittests
+# ./ci.sh <integration> integrationtests
+# ./ci.sh <integration> tests
+#########################################
+
+docker run -t --rm -v ${PWD}:/project 691607105592.dkr.ecr.us-east-1.amazonaws.com/devops-build-cicd:python3.7-buster python run.py ci --integration $@
