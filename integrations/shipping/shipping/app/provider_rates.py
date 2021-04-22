12f9ee12ab611c962226ad2e3419006dd9f3549f
Thomas Holz <tholz>
2021-04-22 13:49:18 +0200

    FAO-103 updated provider rates

--- integrations/shipping/shipping/app/provider_rates.py
+++ integrations/shipping/shipping/app/provider_rates.py
@@ -7,13 +7,13 @@ LOGGER = logging.getLogger(__name__)
 def get_provider_rates(provider_rates_request: ProviderRatesRequest):
     #country_code = provider_rates_request.shipping_address.country_code
     cur_value = provider_rates_request.bag.products[0].price.currency
-    currency = "EUR" if cur_value is None else cur_value
+    currency = "CAD" if cur_value is None else cur_value
 
     # based on country code we need to response the respective provider rates
-    default_standard_shipping = "WMS_DPD_STANDARD"
-    default_express_shipping = "WMS_TNT_EXPRESS"
-    default_pickuppoint_shipping = "WMS_DPD_PICKUPPOINT"
-    default_shiptostore_shipping = "WMS_DPD_SHIPTOSTORE"
+    default_standard_shipping = "STANDARD"
+    default_express_shipping = "EXPRESS"
+    default_pickuppoint_shipping = "WMS_UPS_PICKUPPOINT"
+    default_shiptostore_shipping = "WMS_UPS_SHIPTOSTORE"
 
     #tnt_standard_shipping = "WMS_TNT_STANDARD"
 
