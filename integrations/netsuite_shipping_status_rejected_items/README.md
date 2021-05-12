bdf081899b04aff760cc82ad95ea40feb66a92f9
tfoerster1211 <thomas.foerster@positiveminds.io>
2021-05-12 14:57:53 +0200

    FAO-126 Basic code for item rejections

---
+++ integrations/netsuite_shipping_status_rejected_items/README.md
@@ -0,0 +1,25 @@
+# NetSuite Integration for rejected (non-shipped) items
+
+It can happen that an order should be fulfilled by the DC, but the shipping is rejected, e.g.
+if there is not inventory available or products are damaged. In this case we need to update the
+NST fulfillment / item rejection status and eventually trigger another routing for the item or cancel it.
+
+A saved search is setup in Netsuite to query Sales Orders and Sales Order Items fulfilled from DC which are rejected.
+
+After the search is done, we will update the data in NewStore and update the item with a custom attribute (boolean) that it has been synced to NewStore. This will exclude it from the saved search and won’t be send again.
+
+```
+{ NewStore Event Trigger } ==> `frankandoak-netsuite-rejected-items-to-newstore-trigger`
+     |
+     V
+[ AWS Lambda ] ==> `frankandoak-netsuite_rejected_items_to_newstore`
+     |
+     V
+{ Get Rejected Items from Netsuite via SavedSearch }
+     |
+     V
+[ AWS Lambda ] ==> `frankandoak-netsuite_rejected_items_to_newstore` ==> process each item and mark rejected in NewStore
+     |
+     V
+{ Update each item in NetSuite and mark as processed }
+```
---
+++ integrations/netsuite_shipping_status_rejected_items/netsuite_shipping_status_rejected_items/transformers/retrieve_netsuite_data.py
@@ -0,0 +1,230 @@
+import os
+import logging
+
+from netsuite.service import (
+    TransactionSearchAdvanced,
+    BooleanCustomFieldRef,
+    SalesOrder,
+    CustomFieldList,
+    SearchPreferences,
+    RecordRef
+)
+from netsuite.client import client, make_passport
+from netsuite_shipping_status_rejected_items.utils import Utils
+
+
+LOG_LEVEL_STR = os.environ.get('LOG_LEVEL', 'INFO')
+LOG_LEVEL = logging.DEBUG if LOG_LEVEL_STR.lower() in ['debug'] else logging.INFO
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(LOG_LEVEL)
+
+# Main function to process the rejections
+def process_reject(mapped_data, ns_handler):
+    for item in mapped_data:
+        order_uuid = item['order_id']
+        product_id = item['product_id']
+
+        LOGGER.info(f'process_reject - order_uuid : {order_uuid}, product_id : {product_id}')
+
+        fulfillment_uuid = get_fulfillment_id(order_uuid, product_id, ns_handler)
+
+        if fulfillment_uuid is None:
+            LOGGER.error(f'Could not find fulfillment id for item {product_id} in order {order_uuid}')
+            continue
+
+        LOGGER.info(f'Reject order {order_uuid}, product {product_id}, fulfillment_uuid {fulfillment_uuid}')
+
+        response = False
+        already_shipped = False
+
+        try:
+            response = ns_handler.send_item_rejection(
+                reason="no_inventory",
+                ff_id=fulfillment_uuid,
+                items_json=[product_id])
+        except Exception as ex:  # pylint: disable=broad-except
+            if 'already_shipped' in str(ex):
+                LOGGER.info('Fulfillment request has already been shipped, mark as rejected on RDS and move on.')
+                already_shipped = True
+
+        if not response and not already_shipped:
+            # We remove this item of the mapped_data to not update NetSuite since it didn't work
+            mapped_data.remove(item)
+
+
+# Method to retrieve the rejected items data from the saved seach
+def retrieve_data_from_saved_search(saved_search_id, search_page_size):
+    LOGGER.info(f'savedSearchId - {saved_search_id}')
+
+    search_type = TransactionSearchAdvanced(savedSearchScriptId=saved_search_id)
+
+    try:
+        search_preferences = SearchPreferences(
+            bodyFieldsOnly=False,
+            returnSearchColumns=True,
+            pageSize=search_page_size
+        )
+
+        resp = client.service.search(searchRecord=search_type,
+                                     _soapheaders={
+                                         'searchPreferences': search_preferences,
+                                         'tokenPassport': make_passport()
+                                     })
+
+        LOGGER.debug(f'Search result: {resp}')
+        row_list = resp.body.searchResult.searchRowList
+        if row_list is None:
+            LOGGER.info('Search returned no rejected items. Nothing to process.')
+            return []
+
+        total_pages = resp.body.searchResult.totalPages
+        page_size = resp.body.searchResult.pageSize
+        status = resp.body.searchResult.status
+        search_row = row_list.searchRow
+
+        LOGGER.info(
+            f'Results from Netsuite search - total_pages: {total_pages}, page_size: {page_size}, status: {status}'
+        )
+        LOGGER.debug(f'Search: {search_row}')
+
+        mapped_data = map_netsuite_data(search_row)
+
+        LOGGER.debug(f'retrieve_data_from_saved_search - mapped_data - {mapped_data}')
+    except Exception:  # pylint: disable=broad-except
+        LOGGER.error('Error during the call to the saved search.', exc_info=True)
+        return []
+
+    return mapped_data
+
+
+# Data Mapping on NetSuite Data
+def map_netsuite_data(data):
+    transformed_item_array = []
+    for item in data:
+        try:
+            external_id = item.basic.externalId[0].searchValue.externalId
+            product_id = item.basic.item[0].searchValue.internalId
+            sales_order_internal_id = item.basic.internalId[0].searchValue.internalId
+            sales_order_line = item.basic.line[0].searchValue
+            transformed_item = {
+                "order_id": external_id,
+                "product_id": product_id,
+                "sales_order_internal_id": sales_order_internal_id,
+                "sales_order_line": sales_order_line
+            }
+            transformed_item_array.append(transformed_item)
+        except IndexError:
+            LOGGER.debug(f'Item in search does not appear to be NewStore related: {item}')
+    return transformed_item_array
+
+# Marks the items as synchronized in NewStore at Netsuite, to remove them from the Saved Search
+def mark_rejected_orders_imported(mapped_data):
+    """
+    Extracts the sales order IDs from the mapped search results and calls for them to be updated in NetSuite
+    :param mapped_data: The mapped results of the fulfillment items custom search
+    """
+    for result in mapped_data:
+        internal_id = result['sales_order_internal_id']
+        product_id = result['product_id']
+        line = result['sales_order_line']
+        mark_rejected_order_imported(internal_id, product_id, line)
+
+
+def mark_rejected_order_imported(internal_id, product_id, line):
+    """
+    Updates the rejected sales order synced to newstore flag on a sales order
+    :param internal_id: The ID of the sales order to update
+    """
+    netsuite_config = Utils.get_instance().get_netsuite_config()
+    rejection_flag_script_id = netsuite_config['rejection_processed_flag_script_id']
+
+    try:
+        newstore_synced_field = BooleanCustomFieldRef()
+        newstore_synced_field.scriptId = rejection_flag_script_id
+        newstore_synced_field.value = True
+
+        sales_order = SalesOrder(
+            internalId=internal_id,
+            itemList={
+                'item': [
+                    {
+                        'item': RecordRef(internalId=product_id),
+                        'line': line,
+                        'customFieldList': CustomFieldList([newstore_synced_field])
+                    }
+                ],
+                'replaceAll': False
+            }
+        )
+
+        response = client.service.update(sales_order,
+                                         _soapheaders={
+                                             'tokenPassport': make_passport()
+                                         })
+        LOGGER.debug(f'mark sales order imported - response: {response}')
+        if response.body.writeResponse.status.isSuccess:
+            LOGGER.info(f'Marked sales order with id "{internal_id}" as imported by NewStore in Netsuite.')
+        else:
+            LOGGER.error(f'Failed to mark sales order with id "{internal_id}"'
+                         f' as imported by NewStore in Netsuite. Response: {response.body}')
+    except Exception as e:  # pylint: disable=broad-except
+        LOGGER.error(f'Error while marking the sales order with id "{internal_id}"'
+                     f' as imported by NewStore in Netsuite. Error: {e}')
+
+
+# Gets the ID of the fulfillment of the product
+def get_fulfillment_id(order_id, product_id, ns_handler):
+    distribution_centres = Utils.get_instance().get_distribution_centres()
+
+    order_data = get_newstore_order_data(order_id, ns_handler)
+    if order_data['fulfillmentRequests'] is None:
+        return None
+
+    for fulfillment_request in order_data['fulfillmentRequests']['nodes']:
+        if fulfillment_request['items'] is None:
+            return None
+
+        # the fulfillment location must be the DC to reject items for
+        if fulfillment_request['fulfillmentLocationId'] not in distribution_centres.values():
+            continue
+
+        for item in fulfillment_request['items']['nodes']:
+            if item['productId'] == product_id:
+                return fulfillment_request.id
+
+    return None
+
+
+# Queries the necessary NewStore order data we need for the logic using GraphQL
+def get_newstore_order_data(order_id, ns_handler):
+    graphql_query = """query MyQuery($id: String!, $tenant: String!) {
+  order(id: $id, tenant: $tenant) {
+    id
+    externalId
+    fulfillmentRequests(first: 50) {
+      nodes {
+        items(first: 100) {
+          nodes {
+            id
+            productId
+            rejectionReason
+            shippedAt
+            status
+          }
+        }
+        id
+        fulfillmentLocationId
+      }
+    }
+  }
+}"""
+    data = {
+        "query": graphql_query,
+        "variables": {
+            "id": order_id,
+            "tenant": os.environ.get('TENANT')
+        }
+    }
+
+    graphql_response = ns_handler.graphql_api_call(data)
+    return graphql_response['data']['order']
