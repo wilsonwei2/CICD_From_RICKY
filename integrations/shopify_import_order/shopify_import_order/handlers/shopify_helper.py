1d549a475a477c8d413153f221eb281d54779d4c
Torsten Braun <tbraun@newstore.com>
2021-07-07 12:07:05 +0200

    FAO-177: adjusted service level mapping based on shipping country

--- integrations/shopify_import_order/shopify_import_order/handlers/shopify_helper.py
+++ integrations/shopify_import_order/shopify_import_order/handlers/shopify_helper.py
@@ -12,11 +12,18 @@ def get_order_risks(order_id, shop_id):
     return shopify_connector.get_order_risks(order_id)
 
 
-def get_shipment_service_level(code, title):
-    LOGGER.info(f'Code: {code}; Title: {title}')
-    for shipping_option_key in SHOPIFY_SERVICE_LEVEL_MAP_CONFIG.keys():
-        if (code and code.startswith(shipping_option_key.lower())) \
-        		or (title and title.startswith(shipping_option_key.lower())):
-            return SHOPIFY_SERVICE_LEVEL_MAP_CONFIG[shipping_option_key]
+def get_shipment_service_level(code='', title='', country_code=''):
+    country_code = country_code.upper()
+    LOGGER.info(f'Code: {code}; Title: {title}, Country code: {country_code}')
 
-    return SHOPIFY_SERVICE_LEVEL_MAP_CONFIG['default']
+    if not country_code in SHOPIFY_SERVICE_LEVEL_MAP_CONFIG:
+        country_code = 'INT'
+
+    country_service_levels = SHOPIFY_SERVICE_LEVEL_MAP_CONFIG[country_code]
+
+    for shipping_option_key in country_service_levels.keys():
+        if (code and code.startswith(shipping_option_key.lower())) \
+                or (title and title.startswith(shipping_option_key.lower())):
+            return country_service_levels[shipping_option_key]
+
+    return country_service_levels['default']
