48cc39d03ae707191833741c1bfef2074f139222
tfoerster1211 <48208789+tfoerster1211@users.noreply.github.com>
2021-09-02 17:43:33 +0200

    Feature/historical order import debug (#159)
    * Added debug code - changes not deployed?
    
    * Added another unit test
    
    * Debug - retrieve more than 10 messages
    
    * Reset multiple iterations code

--- integrations/historical_orders_returns/historical_orders_returns/lambdas/sqs_to_newstore.py
+++ integrations/historical_orders_returns/historical_orders_returns/lambdas/sqs_to_newstore.py
@@ -35,34 +35,28 @@ def handler(_, context):
         context=context
     )
 
-    for count in range(50): # pylint: disable=too-many-nested-blocks
-        queue = SQS.get_queue_by_name(QueueName=os.environ['QUEUE_NAME'])
-        messages = queue.receive_messages(
-            MaxNumberOfMessages=10,
-            WaitTimeSeconds=0
-        )
+    messages = QUEUE.receive_messages(
+        MaxNumberOfMessages=10,
+        WaitTimeSeconds=0
+    )
 
-        LOGGER.info(f'Received next 10 messages from queue... iteration {count}')
-        if len(messages) == 0:
-            break
+    for message in messages:
+        try:
+            body = json.loads(message.body)
+            LOGGER.info(f'Processing Message: {message.message_id}')
 
-        for message in messages:
-            try:
-                body = json.loads(message.body)
-                LOGGER.info(f'Processing Message: {message.message_id}')
+            newstore_response = None
+            if TYPE == 'order':
+                newstore_response = import_order_to_newstore(body, ns_handler)
+            if TYPE == 'return':
+                newstore_response = import_return_to_newstore(body['order_id'], body['return'], ns_handler)
 
-                newstore_response = None
-                if TYPE == 'order':
-                    newstore_response = import_order_to_newstore(body, ns_handler)
-                if TYPE == 'return':
-                    newstore_response = import_return_to_newstore(body['order_id'], body['return'], ns_handler)
+            if newstore_response:
+                LOGGER.info('Deleting message from queue...')
+                message.delete()
 
-                if newstore_response:
-                    LOGGER.info('Deleting message from queue...')
-                    message.delete()
-
-            except Exception as e: # pylint: disable=broad-except
-                LOGGER.exception(e)
+        except Exception as e: # pylint: disable=broad-except
+            LOGGER.exception(e)
 
 
     return {
