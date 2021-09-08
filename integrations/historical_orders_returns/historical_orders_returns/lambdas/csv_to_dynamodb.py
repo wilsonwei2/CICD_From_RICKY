460d4608e7bbf17ebd73c4fcb5673606512b72cf
tfoerster1211 <48208789+tfoerster1211@users.noreply.github.com>
2021-09-08 14:45:18 +0200

    Filter voided orders (#167)

--- integrations/historical_orders_returns/historical_orders_returns/lambdas/csv_to_dynamodb.py
+++ integrations/historical_orders_returns/historical_orders_returns/lambdas/csv_to_dynamodb.py
@@ -31,7 +31,7 @@ def process_orders_csv_file(s3_bucket_name, s3_file_key):
         for i, row in enumerate(data):
             # if row has new order id, send order to Dynamodb and create new object
             if i > 0 and current_order_id and row["Name"] != current_order_id:
-                if not is_closed_location(order_obj):
+                if not is_closed_location(order_obj) and not is_voided(order_obj):
                     add_order_to_dynamodb(order_obj, current_order_id)
                 order_obj = get_new_order_obj()
 
@@ -53,7 +53,7 @@ def process_orders_csv_file(s3_bucket_name, s3_file_key):
                 order_obj["shipping"] = row
 
         # send last order to DynamoDB after last row was processed
-        if order_obj and not is_closed_location(order_obj):
+        if order_obj and not is_closed_location(order_obj) and not is_voided(order_obj):
             add_order_to_dynamodb(order_obj, current_order_id)
 
         move_to_archive(obj, s3_bucket_name, s3_file_key)
@@ -92,6 +92,8 @@ def get_new_order_obj():
 def is_closed_location(order_obj):
     return order_obj["details"].get("Store Code", "") in ["BRUST", "LAVST", "MPOST", "EDMST", "QUEST", "VMOST", "LIMST", "SHEST", "OTTST", "METST", "CALST"] # pylint: disable=line-too-long
 
+def is_voided(order_obj):
+    return order_obj["details"].get("Financial Status", "") == "voided"
 
 def get_csv_data(s3_object):
     csvdata = s3_object.get()['Body'].read().decode('utf-8')
