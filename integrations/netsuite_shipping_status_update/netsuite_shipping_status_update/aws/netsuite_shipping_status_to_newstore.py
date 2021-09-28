b430932930b8659ed1572ec48aacc39b9fe66e84
tfoerster1211 <48208789+tfoerster1211@users.noreply.github.com>
2021-09-28 12:38:48 +0200

    Feature/fao 364 (#210)
    * Update netsuite_shipping_status_to_newstore.py
    
    * avoid to send tracking_url if empty

--- integrations/netsuite_shipping_status_update/netsuite_shipping_status_update/aws/netsuite_shipping_status_to_newstore.py
+++ integrations/netsuite_shipping_status_update/netsuite_shipping_status_update/aws/netsuite_shipping_status_to_newstore.py
@@ -278,8 +278,10 @@ def build_newstore_shipment_request(product_ids, tracking_number, carrier):
     shipment['carrier'] = carrier or ''
 
     if shipment['carrier']:
-        shipment['tracking_url'] = get_shipping_carrier_url(
+        tracking_url = get_shipping_carrier_url(
             carrier, tracking_number)
+        if tracking_url:
+            shipment['tracking_url'] = tracking_url
 
     line_items = {}
     line_items['product_ids'] = product_ids
