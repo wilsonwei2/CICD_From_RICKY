69c8752b4fa5494d740b1db5a4112f19dd630967
Christine Ford <christineford@Christines-MacBook-Pro-2.local>
2022-03-09 16:19:09 -0600

    corrected all statment

--- integrations/shopify_import_order/shopify_import_order/aws/receive.py
+++ integrations/shopify_import_order/shopify_import_order/aws/receive.py
@@ -34,7 +34,7 @@ def handler(event, context): # pylint: disable=W0613
         LOGGER.debug(f'tags: {existing_tags}')
         stop_retry_tags = os.environ.get('STOP_RETRY_TAGS', 'newstore_created,newstore_failed').split(",")
         LOGGER.debug(f'STOP_RETRY_TAGS: {stop_retry_tags}')
-        if all(tag not in stop_retry_tags for tag in existing_tags):
+        if any(tag in stop_retry_tags for tag in existing_tags):
             LOGGER.debug(f'Not processing Order with id {order_id} -- request because it has {existing_tags} - tags')
             return {
                 'statusCode': 200,
@@ -43,7 +43,7 @@ def handler(event, context): # pylint: disable=W0613
         # if 'signifyd approved' is not in tags, send 500 so Shopify will retry later
         wait_to_receive_tags = os.environ.get('RETRY_TAGS', 'signifyd approved').split(",")
         LOGGER.debug(f'RETRY_TAGS: {wait_to_receive_tags}')
-        if any(tag in wait_to_receive_tags for tag in existing_tags):
+        if all(tag not in wait_to_receive_tags for tag in existing_tags):
             LOGGER.info(f'Not processing order with {order_id}. It is not signifyd approved. Senging 500 back to Shopify')
             return {
                 'statusCode': 500,
