9e0eef644cc17a6f91003655e3e99ab9c85c86c3
Srinivas Kalyani <srinivas.kalyani@changecx.com>
2022-07-10 22:04:05 +0530

    disable shopify coupons

--- integrations/yotpo_shopper_loyalty/yotpo_shopper_loyalty/aws/process_order.py
+++ integrations/yotpo_shopper_loyalty/yotpo_shopper_loyalty/aws/process_order.py
@@ -4,6 +4,7 @@ import json
 from lambda_utils.sqs.SqsHandler import SqsHandler
 from yotpo_shopper_loyalty.handlers.utils import Utils
 from yotpo_shopper_loyalty.handlers.yotpo_handler import YotpoHandler
+from yotpo_shopper_loyalty.handlers.shopify_gql import ShopifyDiscountCodeGQL
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
@@ -13,11 +14,6 @@ SQS_HANDLER = SqsHandler(os.environ['SQS_YOTPO_ORDER_OPENED'])
 NEWSTORE_HANDLER = None
 
 def handler(event, context):  # pylint: disable=unused-argument
-    """
-    Start of the lambda handler
-    :param event: Shopify webhook order json
-    :param context: function context
-    """
     LOGGER.info(f'Processing return Event: {event}')
     utils = Utils.get_instance()
     global NEWSTORE_HANDLER  # pylint: disable=W0603
@@ -32,32 +28,30 @@ def handler(event, context):  # pylint: disable=unused-argument
             else:
                 LOGGER.error(f'Failed to process event: {record["body"]}')
         except Exception as ex:  # pylint: disable=broad-except
-            LOGGER.exception('Some error happen while processing the return')
+            LOGGER.exception('Some error occurred while processing the order w.r.t Yotpo')
             err_msg = str(ex)
             LOGGER.error(f'Error message: {err_msg}')
             continue
 
 def _process_order(order_json):
-    """
-    Process the order json
-    :param order_json: Shopify webhook order json
-    :return: True if the order was processed successfully, False otherwise
-    """
     LOGGER.info(f'Processing order: {order_json}')
     order_payload = json.loads(order_json).get('payload', {})
+    discounts = order_payload.get('discounts', [])
+    coupon_codes = list([discount.get('coupon_code') for discount in discounts
+                         if (discount.get('coupon_code').startswith("FAOU") or discount.get('coupon_code').startswith("FAOC"))])
     if (order_payload.get('channel_type')) == 'web':
-        _disable_ns_coupons(order_payload)
+        _disable_ns_coupons(coupon_codes)
     elif (order_payload.get('channel_type')) == 'store':
-        _disable_shopify_coupons(order_payload)
+        _disable_shopify_coupons(coupon_codes)
         _create_yotpo_order(order_payload)
     return False
 
-def _disable_ns_coupons(order_payload):
-    discounts = order_payload.get('discounts', [])
-    coupons = list([discount.get('coupon_code') for discount in discounts
-                    if (discount.get('coupon_code').startswith("FAOU") or discount.get('coupon_code').startswith("FAOC"))])
-    LOGGER.info(f'Disabling coupons: {coupons}')
-    coupon_items_to_disable = NEWSTORE_HANDLER.get_coupons(coupons)
+def _disable_ns_coupons(coupon_codes):
+    if not coupon_codes:
+        LOGGER.info('No coupons to disable on Newstore')
+        return True
+    LOGGER.info(f'Disabling coupons: {coupon_codes}')
+    coupon_items_to_disable = NEWSTORE_HANDLER.get_coupons(coupon_codes)
     LOGGER.info(f'Get Coupons response: {coupon_items_to_disable}')
     coupon_ids_to_disable = [coupon_item.get('id') for coupon_item in coupon_items_to_disable]
     LOGGER.info(f'Disabling coupons: {coupon_ids_to_disable}')
@@ -66,8 +60,14 @@ def _disable_ns_coupons(order_payload):
         LOGGER.info(f'Disabled coupon: {coupon_id}')
 
 
-def _disable_shopify_coupons(order_payload): # pylint: disable=W0613
-    pass
+def _disable_shopify_coupons(coupon_codes):
+    if not coupon_codes:
+        LOGGER.info('No coupons to disable on Shopify')
+        return True
+    shopify_discount_gql = ShopifyDiscountCodeGQL()
+    for coupon in coupon_codes:
+        LOGGER.info(f'Disabling coupon: {coupon}')
+        shopify_discount_gql.disable_shopify_coupon(coupon)
 
 def _create_yotpo_order(order_payload):
     LOGGER.info(f'Creating yotpo order: {order_payload}')
---
+++ integrations/yotpo_shopper_loyalty/yotpo_shopper_loyalty/handlers/shopify_gql.py
@@ -0,0 +1,120 @@
+"""
+Our sincere gratitude to the author of the following post for providing the basis for this class.
+https://community.shopify.com/c/shopify-apis-and-sdks/python-graphql-example-with-rate-limiting-and-error-checking/td-p/713261
+"""
+
+import json
+import time
+import requests
+import logging
+
+from yotpo_shopper_loyalty.handlers.utils import Utils
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+
+class ShopifyDiscountCodeGQL:
+    """
+    Class to disable Shopify coupons using the
+    Shopify GraphQL Admin API.
+    """
+    def __init__(self) -> None:
+        self.api_version = '2022-07'
+        self.utils = Utils.get_instance()
+        self.param_store = self.utils.get_parameter_store()
+        self.shopify_config = self.param_store.get_param('shopify')
+        self.api_key = self.shopify_config['username']
+        self.password = self.shopify_config['password']
+        self.shop = self.shopify_config['shop']
+        self.shop_url = f'https://{self.api_key}:{self.password}@{self.shop}.myshopify.com/admin/api/{self.api_version}'
+
+    def disable_shopify_coupon(self, code: str) -> None:
+        """
+        This is the main calling function that is exposed
+        in order to disable a coupon.
+        """
+        coupon_id = self._get_coupon_id_by_code(code)
+        return self._disable_coupon_gql(coupon_id)
+
+    def _get_coupon_id_by_code(self, code: str) -> str:
+        """
+        Get the coupon ID by code. This coupon ID is required to
+        disable a coupon.
+        """
+        query = f"""
+            {{
+                codeDiscountNodeByCode(code:"{code}") {{
+                    codeDiscount
+                    id
+                }}
+            }}
+        """
+        LOGGER.info(f'Get coupon GQL query: {query}')
+        response = self._call_shopify_gql(query)
+        return response['codeDiscountNodeByCode']['id']
+
+    def _disable_coupon_gql(self, coupon_id: str) -> None:
+        """
+        Disable a coupon using the Shopify GraphQL Admin API.
+        A coupon ID is required to disable a coupon.
+        """
+        mutation = """
+        mutation discountCodeDeactivate($id:ID!) {
+            discountCodeDeactivate(id: $id) {
+                codeDiscountNode {
+                codeDiscount {
+                    ... on DiscountCodeBasic {
+                      title
+                      status
+                      startsAt
+                      endsAt
+                    }
+                }
+            }
+                userErrors {
+                    field
+                    code
+                    message
+                }
+            }
+        }
+        """
+        variables = {"id": coupon_id}
+        LOGGER.info(f'Disable coupon GQL query: {mutation}')
+        LOGGER.info(f'Disable coupon GQL variables: {variables}')
+        response = self._call_shopify_gql(mutation, variables)
+        LOGGER.info(f'Disable coupon GQL response: {response}')
+        if response['discountCodeDeactivate']['codeDiscountNode']['codeDiscount']['status'] is not 'EXPIRED':
+            raise ValueError('Coupon not expired')
+
+    def _call_shopify_gql(self, gql_string: str, variables=None):
+        """
+        This function makes the actual API call, it also handles errors and
+        rate limiting.
+        """
+        response = requests.post(f'{self.shop_url}/graphql.json', json={'query': gql_string, 'variables':variables})
+        if response.status_code == 400:
+            raise ValueError('GraphQL error:' + response.text)
+        answer = json.loads(response.text)
+        throttle_status = None
+        if 'errors' in answer:
+            try:
+                throttle_status = answer['errors'][0]['extensions']['code']
+            except KeyError:
+                pass
+            if throttle_status != 'THROTTLED':
+                raise ValueError('GraphQL error:' + repr(answer['errors']))
+        ql_required = answer['extensions']['cost']['requestedQueryCost']
+        ql_limit = answer['extensions']['cost']['throttleStatus']['maximumAvailable']
+        ql_avail = answer['extensions']['cost']['throttleStatus']['currentlyAvailable']
+        LOGGER.info(f'GraphQL throttling status: {str(ql_avail)}/{str(int(ql_limit))}')
+        while throttle_status == 'THROTTLED':
+            LOGGER.info(f'Waiting (GraphQL throttling)...{str(ql_avail)}/{str(int(ql_limit))} used, requested {str(ql_required)}')
+            time.sleep(1)
+            response = requests.post(f'{self.shop_url}/graphql.json', json={'query': gql_string, 'variables':variables})
+            answer = json.loads(response.text)
+            try:
+                throttle_status = answer['errors'][0]['extensions']['code']
+            except KeyError:
+                throttle_status = None
+        return answer['data']
