0093d5bbc0abef998f2c25ec9c32e247e59bbb13
karthik Yanagandula <kyanagandula@gmail.com>
2022-11-03 11:19:46 -0700

    fixed if condition

--- integrations/shopify_import_order/shopify_import_order/orders.py
+++ integrations/shopify_import_order/shopify_import_order/orders.py
@@ -496,8 +496,8 @@ def _get_shipping_option(order, shipping_offer_token):
             'price': 0.0,
             'tax': 0.0
         }
-    
-    if shipping_address['state'] in ['HI', 'AK'] or 'po box' in shipping_address['address_line_1'].replace('.','').lower():
+    address_line_1 =  shipping_address['address_line_1'].replace('.','').lower()
+    if shipping_address['state'] in ['HI', 'AK'] or 'po box' in address_line_1:
         shipping_option['service_level_identifier'] = 'EXPRESS_POST_USA'
 
     LOGGER.debug(f'Returned shipping option is {shipping_option}')
