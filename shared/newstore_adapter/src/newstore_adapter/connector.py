1441a8c10d38077136d0339afd5e6f5a323a2519
rickyyu <ricky.yu@frankandoak.com>
2023-08-02 17:50:24 -0400

    add logs for debug

--- shared/newstore_adapter/src/newstore_adapter/connector.py
+++ shared/newstore_adapter/src/newstore_adapter/connector.py
@@ -756,6 +756,7 @@ class NewStoreConnector(object):
     def start_availability_export(self, last_updated_at=None):
         url = 'https://%s/v0/d/availabilities/bulk' % (self.host)
         try:
+            logger.info('last_updated_at', last_updated_at)
             response = self.newstore_adapter.post_request(url, (
                 {'last_updated_at': int(last_updated_at)} if last_updated_at else {}
             ))
